4a4b86e278a26a172ce672faa131fe651ccb6fb5
Rokhdelar <testkugua@hotmail.com>
2015-06-12 17:01:32 +0800

    Initial commit

---
+++ app/src/main/java/com/x/rokhdelar/xsshtool/MainActivity.java
@@ -0,0 +1,84 @@
+package com.x.rokhdelar.xsshtool;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listViewMain=(ListView)findViewById(R.id.mainListView);
+        final ArrayList<HashMap<String,Object>> arrayListMainItem=new ArrayList<>();
+        HashMap<String,Object> map=new HashMap<String,Object>();
+
+        map.put("itemImage",R.mipmap.ic_sw8905);
+        map.put("itemCaption", "交换机管理");
+        map.put("itemDescription", "交换机相关操作，端口、mac、vlan等...");
+        arrayListMainItem.add(map);
+
+        map=new HashMap<>();
+        map.put("itemImage",R.mipmap.ic_hwolt);
+        map.put("itemCaption", "华为OLT管理");
+        map.put("itemDescription", "华为OLT相关操作，端口、mac、vlan等...");
+        arrayListMainItem.add(map);
+
+        map=new HashMap<>();
+        map.put("itemImage",R.mipmap.ic_zteolt);
+        map.put("itemCaption", "中兴OLT管理");
+        map.put("itemDescription", "中兴OLT相关操作，端口、mac、vlan等...");
+        arrayListMainItem.add(map);
+
+        SimpleAdapter simpleAdapterMainItem=new SimpleAdapter(this,
+                arrayListMainItem,
+                R.layout.mainitem,
+                new String[]{"itemImage","itemCaption","itemDescription"},
+                new int[]{R.id.itemImage,R.id.itemCaption,R.id.itemDescription});
+
+        listViewMain.setAdapter(simpleAdapterMainItem);
+
+        listViewMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"您选择了"+position,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
